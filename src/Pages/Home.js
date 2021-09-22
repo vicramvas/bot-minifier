69cc7e9eb5301f79bb05ed553a300e533574d5ea
P Vignesh <vicramvas@gmail.com>
2021-09-22 19:52:58 +0000

    Added one line chart with data from API

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,15 +1,27 @@
 import React from 'react';
+import axios from 'axios';
 import { LineChart } from '../Components/Charts/LineChart';
 import { PolarAreaChart } from '../Components/Charts/PolarAreaChart';
 import { RadarChart } from '../Components/Charts/RadarChart';
 import { DoughnutChart } from '../Components/Charts/DoughnutChart'
 
+const covidURL = 'https://data.covid19india.org/v4/min/timeseries.min.json'
 const Home = () => {
+
+    const [covStatus, setStatus]= React.useState(null);
+
+    React.useEffect(()=>{
+        axios.get(covidURL).then((response)=>{
+            setStatus(response.data)
+        });
+    },[]);
+    if(!covStatus) return null;
+    console.log(covStatus);
     return(
         <section className="row">
             <div className='col-6 card'>
             <h1>Chart</h1>
-                <LineChart/>
+                <LineChart data={covStatus}/>
             </div>
              <div className='col-6 card'>
              <h1>Chart</h1>
